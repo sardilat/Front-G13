09ba25d646ee7bbd4631872c7a97bcb5f479ba75
Sebastian Ardila Trujillo <ardilatruji@gmail.com>
2022-12-22 11:23:55 -0500

    Corrección back.js

--- src/conexiones/back.js
+++ src/conexiones/back.js
@@ -1,10 +1,6 @@
 module.exports = {
     api: {
         baseURL: "http://129.80.191.6:8080"
-<<<<<<< HEAD
         //baseURL: "http://localhost:4000"
-=======
-        
->>>>>>> 2088166abb58f3a8a3853b6f231b23c027e2f499
     }
 }
